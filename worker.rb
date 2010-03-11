61f7cb940f770a7882c1deed71bc7b0a3cd01e84
Edwin <edds718@gmail.com>
2010-03-11 15:12:01 -0500

    added parsing of multiple rss feeds

--- worker.rb
+++ worker.rb
@@ -4,31 +4,38 @@ require 'rss/1.0'
 require 'rss/2.0'
 require 'open-uri'
 
-#test rss feed for RubyInside
-
-feed="http://feeds.feedburner.com/RubyInside"
-content=""
-open(feed) do |f| content = f.read end
-
-rss = RSS::Parser.parse(content, false)
+rssfeeds = [ "http://feeds.feedburner.com/RubyInside", "http://feeds.feedburner.com/ElcCodeFeed"]
 
 
 
-#print "RSS title: ", rss.channel.title, "\n"
-#print "RSS link: ", rss.channel.link, "\n"
-#print "RSS description: ", rss.channel.description, "\n"
-#print "RSS publication date: ", rss.channel.date, "\n"
 
-rsscount = 0
 
-while rsscount < rss.items.size
 
-	print "title: ", rss.items[rsscount].title, "\n"
-	print "link: ", rss.items[rsscount].link, "\n\n"
-	
-	rsscount += 1
 
+for i in rssfeeds
+  
+    #  feed="http://feeds.feedburner.com/RubyInside"
+       content=""
+       open(i) do |f| content = f.read end
+
+       rss = RSS::Parser.parse(content, false)
+
+
+    rsscount = 0
+
+    while rsscount < rss.items.size
+      
+      #replace with webkit2png 
+    	print "creating a screenshot of . . . ", rss.items[rsscount].link, "\n\n"
+    	rsscount += 1
+
+    end
+  
+  
 end
+
+
+
 
 
 
